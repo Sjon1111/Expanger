a9ec841570d1e5d497944b0ae78f9309fd65c375
Sandip Prajapati <145475256+Sjon1111@users.noreply.github.com>
2025-05-28 22:01:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
 
-=======
-    base: '/',
->>>>>>> 705c014320ee08d57a083fab5380eb7f1a4f9fec
   plugins: [react()],
 },
   {
