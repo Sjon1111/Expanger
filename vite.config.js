fc2cfc08709cebee900b0e712bc58aea0e44c21c
Sjon1111 <sjon22557@gmail.com>
2025-05-28 22:19:28 +0530

    change file

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config/ change code
 export default defineConfig({
-
   plugins: [react()],
 },
   {
